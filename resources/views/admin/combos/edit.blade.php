e24a5c8406405cd7342f4fb64c7cfec5b583ebdc
Augusto <yefferson.mod@gmail.com>
2024-05-13 18:33:53 -0500

    fix combos

--- resources/views/admin/combos/edit.blade.php
+++ resources/views/admin/combos/edit.blade.php
@@ -17,34 +17,36 @@
 <div class="row">
     <div class="col-12">
         <div class="card border-primary">
-            <form action="/agendarPost" method="POST">
-                @csrf
-                <div class="mb-3">
-                    <input type="text" class="form-control" id="t11combo" name="t11combo" value="{{ $registro->t11id }}" hidden >
-                </div>
-                <div class="mb-3">
-                    <label for="t11nombre" class="form-label">Nombre de la persona a Entregar</label>
-                    <input type="text" class="form-control" id="t11nombre" name="t11nombre" placeholder="Ingresa tu nombre" value="{{  $registro->t11nombre }}" required>
-                </div>
-                <div class="mb-3">
-                    <label for="t11numero" class="form-label">Telefono de la persona a Entregar</label>
-                    <input type="number" class="form-control" id="t11numero" name="t11numero" placeholder="Ingresa el Telefono" value="{{  $registro->t11numero }}" required>
-                </div>
-                <div class="col-md-2">
-                    <label for="t11hora">Hora de entrega</label>
-                    <select id="t11hora" name="t11hora" class="form-control">
-                        @foreach($horas as $hora)
-                            <option value="{{ $hora }}">{{ $hora }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="t11direccion" class="form-label">Direccion</label>
-                    <textarea class="form-control" id="t11direccion" name="t11direccion" rows="3" placeholder="Escribe la direccion de entrega" value="{{  $registro->t11direccion }}" required></textarea>
-                </div>
+            <div class="col-10">
+                <form action="/agendarPost" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <input type="text" class="form-control" id="t11combo" name="t11combo" value="{{ $registro->t11id }}" hidden >
+                    </div>
+                    <div class="mb-3">
+                        <label for="t11nombre" class="form-label">Nombre de la persona a Entregar</label>
+                        <input type="text" class="form-control" id="t11nombre" name="t11nombre" placeholder="Ingresa tu nombre" value="{{  $registro->t11nombre }}" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="t11numero" class="form-label">Telefono de la persona a Entregar</label>
+                        <input type="number" class="form-control" id="t11numero" name="t11numero" placeholder="Ingresa el Telefono" value="{{  $registro->t11numero }}" required>
+                    </div>
+                    <div class="col-md-2">
+                        <label for="t11hora">Hora de entrega</label>
+                        <select id="t11hora" name="t11hora" class="form-control">
+                            @foreach($horas as $hora)
+                                <option value="{{ $hora }}">{{ $hora }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="t11direccion" class="form-label">Direccion</label>
+                        <textarea class="form-control" id="t11direccion" name="t11direccion" rows="3" placeholder="Escribe la direccion de entrega" value="{{  $registro->t11direccion }}" required></textarea>
+                    </div>
 
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </form>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
