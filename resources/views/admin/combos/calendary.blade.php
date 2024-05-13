3986e826dd9761c60f5d841fb9637a84eee65063
Augusto <yefferson.mod@gmail.com>
2024-05-13 16:01:55 -0500

    fix combos

--- resources/views/admin/combos/calendary.blade.php
+++ resources/views/admin/combos/calendary.blade.php
@@ -49,7 +49,8 @@
     document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
+          initialView: 'dayGridMonth',
+          events: @json($events)
         });
         calendar.render();
       });
