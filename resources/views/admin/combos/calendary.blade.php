48d2735903e0f60cf67422b87115da8903f0a7e6
Augusto <yefferson.mod@gmail.com>
2024-05-13 16:13:21 -0500

    fix combos

--- resources/views/admin/combos/calendary.blade.php
+++ resources/views/admin/combos/calendary.blade.php
@@ -26,11 +26,13 @@
 @stop
 
 @section('css')
-
+<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
+<link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
 @stop
 
 @section('js')
-<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js"></script>
+<script src='fullcalendar/dist/index.global.js'></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js"></script> --}}
 <script src="{{ asset('public/vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js') }}"></script>
 <script type="text/javascript">
     $(document).ready(function() {
@@ -46,14 +48,17 @@
     });
 
 
+
     document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
-            center: 'title',
-            initialView: 'dayGridMonth',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth',
+            themeSystem: 'bootstrap5'
             events: @json($events)
         });
+        // var calendar = new FullCalendar.Calendar(calendarEl, {
+        //     initialView: 'dayGridMonth',
+        //     events: @json($events)
+        // });
         calendar.render();
       });
 
