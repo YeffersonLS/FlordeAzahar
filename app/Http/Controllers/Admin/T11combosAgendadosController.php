4aff30d2ed8823f42a02f89120bbd8112a0adc5d
Augusto <yefferson.mod@gmail.com>
2024-05-13 18:06:43 -0500

    fix combos

--- app/Http/Controllers/Admin/T11combosAgendadosController.php
+++ app/Http/Controllers/Admin/T11combosAgendadosController.php
@@ -96,7 +96,7 @@ class T11combosAgendadosController extends Controller
             $events[] = [
                 'title' => $registro->t11nombre,
                 'start'=> $registro->t10vencimiento.' '.$registro->t11hora,
-                'eventBackgroundColor'=> "red" // Color de fondo rojo
+                'eventColor'=> "red" // Color de fondo rojo
 
             ];
             $titulo = $registro->t10nombre;
