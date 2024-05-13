5668fe090ce005037b5fe6ed84916d69cf140c7d
Augusto <yefferson.mod@gmail.com>
2024-05-13 01:42:47 -0500

    fix agendar combos

--- app/Http/Controllers/FuncionesFront.php
+++ app/Http/Controllers/FuncionesFront.php
@@ -6,12 +6,13 @@ use Illuminate\Http\Request;
 
 class FuncionesFront extends Controller
 {
-    public static function horaEntregaCombos (){
+    public static function horaEntregaCombos() {
 
         $horas = [];
 
-        for ($i=9; $i < 18; $i++) {
-            $horas[] = $i;
+        for ($i = 9; $i < 18; $i++) {
+            $horaFormateada = str_pad($i, 2, '0', STR_PAD_LEFT) . ':00';
+            $horas[] = $horaFormateada;
         }
 
         return $horas;
