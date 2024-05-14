6ccb49775a95a65e4d1a033971926a135dc5c8fb
Augusto <yefferson.mod@gmail.com>
2024-05-14 09:46:34 -0500

    fix cart

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -34,20 +34,21 @@
             <h2>{{ $product->t02nombre }}</h2>
             <p>{!! $product->t04descripcion !!}</p>
             <p>Precio: ${{ $product->t04precio }}</p>
-             {{-- <form action="{{ route('cart.add', ['product' => $product->t04id]) }}" method="POST">
-                @csrf
-                <button type="submit" class="btn btn-primary">Agregar al carrito</button>
-            </form> --}}
             <div class="mt-3">
                 @foreach ($images as $img)
                 <img src="{{ asset($img->image_path) }}" alt="{{ $product->t02nombre }}" class="thumbnail" onclick="changeImage('{{ $img->image_path }}')" style="max-width: 100px; max-height: 100px;">
                 @endforeach
             </div>
             <div class="mt-3">
-                <label for="quantity">Cantidad:</label>
-                <input type="number" name="quantity" id="quantity" min="1" value="1">
+                <form action="{{ route('cart.add', ['product' => $product->t04id], ['quantity'=> 'quantity']) }}" method="POST">
+                    @csrf
+                    <label for="quantity">Cantidad:</label>
+                    <input type="number" name="quantity" id="quantity" min="1" value="1">
+                    <button type="submit" class="btn btn-primary">Agregar al carrito</button>
+                </form>
 
-                <button type="submit" id="guardar" class="btn btn-success"><i class="fa fa-save"></i> Añadir al carrito</button>
+
+                {{-- <button type="submit" id="guardar" class="btn btn-success"><i class="fa fa-save"></i> Añadir al carrito</button> --}}
             </div>
         </div>
     </div>
@@ -61,39 +62,39 @@
         document.getElementById('mainImage').src = "{{ asset('') }}" + imagePath;
     };
 
-    $(document).ready(function() {
-    $("#guardar").click(function() {
-        const addToCartForm = document.querySelector('form[action="{{ route('cart.add') }}"]');
+    // $(document).ready(function() {
+    // $("#guardar").click(function() {
+    //     const addToCartForm = document.querySelector('form[action="{{ route('cart.add') }}"]');
 
-            addToCartForm.addEventListener('guardar', (event) => {
-                event.preventDefault(); // Prevent default form submission
+    //         addToCartForm.addEventListener('guardar', (event) => {
+    //             event.preventDefault(); // Prevent default form submission
 
-                const productId = document.getElementById('t04id').value;
-                const quantity = document.getElementById('quantity').value;
+    //             const productId = document.getElementById('t04id').value;
+    //             const quantity = document.getElementById('quantity').value;
 
-                // Send the product ID and quantity to the controller using an AJAX request
-                fetch('{{ route('cart.add') }}', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRF-TOKEN': '{{ csrf_token() }}' // Add CSRF token for security
-                    },
-                    body: JSON.stringify({
-                        product_id: productId,
-                        quantity: quantity
-                    })
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        // Handle the response from the controller (e.g., display a success message)
-                        console.log('Producto añadido al carrito:', data);
-                    })
-                    .catch(error => {
-                        console.error('Error al añadir al carrito:', error);
-                    });
-            });
-        });
-    });
+    //             // Send the product ID and quantity to the controller using an AJAX request
+    //             fetch('{{ route('cart.add') }}', {
+    //                 method: 'POST',
+    //                 headers: {
+    //                     'Content-Type': 'application/json',
+    //                     'X-CSRF-TOKEN': '{{ csrf_token() }}' // Add CSRF token for security
+    //                 },
+    //                 body: JSON.stringify({
+    //                     product_id: productId,
+    //                     quantity: quantity
+    //                 })
+    //             })
+    //                 .then(response => response.json())
+    //                 .then(data => {
+    //                     // Handle the response from the controller (e.g., display a success message)
+    //                     console.log('Producto añadido al carrito:', data);
+    //                 })
+    //                 .catch(error => {
+    //                     console.error('Error al añadir al carrito:', error);
+    //                 });
+    //         });
+    //     });
+    // });
 </script>
 
 @endsection
