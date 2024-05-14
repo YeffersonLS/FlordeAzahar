e059b539a354d52ade79bb9b7628a6c3f5d5abd2
Augusto <yefferson.mod@gmail.com>
2024-05-13 19:03:53 -0500

    fix combos

--- resources/views/admin/combos/edit.blade.php
+++ resources/views/admin/combos/edit.blade.php
@@ -18,7 +18,7 @@
     <div class="col-12">
         <div class="card border-primary">
             <div class="col-10">
-                <form action="{{ route('admin.diary.update', ['diary' => $registro->t11id]) }}" method="PUT">
+                <form action="{{ route('admin.diary.update', ['diary' => $registro->t11id]) }}" method="PATCH">
                     @csrf
 
                     <div class="col-md-4">
