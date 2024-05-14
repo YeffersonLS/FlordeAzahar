1448b6367d80bc5cb7a058bf691a3c302fce825b
Augusto <yefferson.mod@gmail.com>
2024-05-14 01:31:05 -0500

    fix combos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -149,7 +149,7 @@
 
                     <div>
                         <a href="/carrito" style="text-decoration: none; color: inherit;">
-                            <span style="font-size: 1.2em;"></span> Carrito
+                            <span style="font-size: 1.2em;"></span>   Carrito
                         </a>
                     </div>
 
