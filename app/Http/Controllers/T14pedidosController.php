a26b48cc179b15d41999e46e456576d6f22cf982
Augusto <yefferson.mod@gmail.com>
2024-05-16 17:10:06 -0600

    fix cart

--- app/Http/Controllers/T14pedidosController.php
+++ app/Http/Controllers/T14pedidosController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class T14pedidosController extends Controller
 {
     public function pay(Request $request){
-        dd($request->all());
+        // dd($request->all());
+        $user = auth()->user();
+        if (is_null($user)) {
+            dd('no hay usuario');
+        }
+
+        dd('se paso');
     }
 }
