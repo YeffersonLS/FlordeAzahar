c99c64dff13a753194463902f7372e489306c4f4
Augusto <yefferson.mod@gmail.com>
2024-05-18 00:01:29 -0600

    fix cart

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        // dd($data);
+        dd($data);
 
         return User::create([
             'sys01name' => $data['sys01name'],
