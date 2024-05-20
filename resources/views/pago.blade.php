95c39fbf7b172d69b9299c726a65b2348601dbf3
Augusto <yefferson.mod@gmail.com>
2024-05-19 20:33:27 -0600

    fix pay cart

--- resources/views/pago.blade.php
+++ resources/views/pago.blade.php
@@ -12,19 +12,27 @@
                 <div class="form-group">
                     <label for="t14tipopago">Forma de Pago:</label>
                     <select name="t14tipopago" id="t14tipopago" class="form-control">
-                        <option value=""></option>
+                        <option value="">seleccione una forma de pago</option>
                         <option value="efectivo">Efectivo</option>
                         <option value="transferencia">Transferencia Bancaria</option>
                     </select>
                 </div>
-
+                <div class="row form-group">
+                    <div class="col-md-12 text-center">
+                        <p>Estos son los datos para la transferencia bancaria y confirmar tu pedido</p>
+                    </div>
+                </div>
                 <div class="col-md-12">
                     <label for="t14direccion">Dirección de Envío:</label>
                     <textarea name="t14direccion" id="t14direccion" class="form-control" rows="3"></textarea>
                 </div>
 
-                <div class="col-md-12">
-                    <button type="submit" class="btn btn-success">Confirmar Pedido</button>
+                <div class="row form-group">
+                    <div class="col-md-12 text-center">
+                        <a href="{!! url('pago') !!}" class="btn btn-warning"><i class="fa fa-reply"></i> Cancelar</a>
+                        <button type="submit" id="guardar" class="btn btn-success"><i class="fa fa-save"></i> Confirmar Pedido</button>
+                        <input type="submit" id="btnSubmit" style="display: none;">
+                    </div>
                 </div>
             </form>
         </div>
@@ -36,5 +44,14 @@
 
 <script>
     // alert('holra');
+    <script type="text/javascript">
+    $(document).ready(function() {
+
+
+        $("#guardar").click(function(){
+            $("#btnSubmit").click();
+        });
+    });
+</script>
 </script>
 @endsection
