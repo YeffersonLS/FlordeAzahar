c9d83e49fd78a06be5e9440c4252fdc7e5f01b7b
Augusto <yefferson.mod@gmail.com>
2024-05-19 20:05:29 -0600

    fix pay cart

--- resources/views/pago.blade.php
+++ resources/views/pago.blade.php
@@ -2,27 +2,32 @@
 
 @section('content')
 <div class="container">
-    <h1>Confirma tu forma de Pago</h1>
+    <div class="row">
+        <div class="col-12">
+            <h1>Confirma tu forma de Pago</h1>
 
-    <form action="{{ route('payPost.cart') }}" method="POST">
-        @csrf
+            <form action="{{ route('payPost.cart') }}" method="POST">
+                @csrf
 
-        <div class="form-group">
-            <label for="t14tipopago">Forma de Pago:</label>
-            <select name="t14tipopago" id="t14tipopago" class="form-control">
-                <option value="efectivo">Efectivo</option>
-                <option value="transferencia">Transferencia Bancaria</option>
-            </select>
-        </div>
+                <div class="form-group">
+                    <label for="t14tipopago">Forma de Pago:</label>
+                    <select name="t14tipopago" id="t14tipopago" class="form-control">
+                        <option value="efectivo">Efectivo</option>
+                        <option value="transferencia">Transferencia Bancaria</option>
+                    </select>
+                </div>
 
-        <div class="form-group">
-            <label for="t14direccion">Dirección de Envío:</label>
-            <textarea name="t14direccion" id="t14direccion" class="form-control" rows="3"></textarea><br>
+                <div class="form-group">
+                    <label for="t14direccion">Dirección de Envío:</label>
+                    <textarea name="t14direccion" id="t14direccion" class="form-control" rows="3"></textarea>
+                </div>
+
+                <div>
+                    <button type="submit" class="btn btn-success">Confirmar Pedido</button>
+                </div>
+            </form>
         </div>
-        <div class="form-group">
-            <button type="submit" class="btn btn-success">Confirmar Pedido</button>
-        </div>
-    </form>
+    </div>
 </div>
 @endsection
 
