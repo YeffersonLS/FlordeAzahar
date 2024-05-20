d76f2ec6dd8df49522bd3880c1e4790433b5b81f
Augusto <yefferson.mod@gmail.com>
2024-05-19 21:20:48 -0600

    fix pay cart

--- resources/views/pago.blade.php
+++ resources/views/pago.blade.php
@@ -22,7 +22,13 @@
                         <p>Estos son los datos para la transferencia bancaria y confirmar tu pedido</p>
                         <img src="https://heladeriaflordeazahar.com/public/storage/images/t06banners/WhatsApp%20Image%202024-05-13%20at%2018.47.44.jpeg" alt="Datos Bancarios" id="datosBancarios">
                         <p style="text-decoration: solid;"><b>Escribenos al whatsapp para confirmar tu transferencia<a href="https://wa.link/cxvdva">229 667 4807</a></b></p>
-
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-md-12 text-center" id="efectivo">
+                        <p>Puedes ir al local y pagar en efectivo o puedes pedir que el domiciliario recoja el efectivo en tu casa</p>
+                        {{-- <img src="https://heladeriaflordeazahar.com/public/storage/images/t06banners/WhatsApp%20Image%202024-05-13%20at%2018.47.44.jpeg" alt="Datos Bancarios" id="datosBancarios"> --}}
+                        <p style="text-decoration: solid;"><b>Escribenos al whatsapp para confirmarnos que opcion es la mas comoda<a href="https://wa.link/p15czk">229 667 4807</a></b></p>
                     </div>
                 </div>
                 <div class="col-md-12">
@@ -49,16 +55,27 @@
 <script type="text/javascript">
     $(document).ready(function() {
         $('#banco').parent().hide();
-
+        $('#efectivo').parent().hide();
 
         $('#t14tipopago').on('change', function(){
             console.log($(this).val());
 
             if($(this).val() == 'transferencia'){
                 $('#banco').parent().show();
+                $('#efectivo').parent().hide();
+
             }else{
                 $('#banco').parent().hide();
+                $('#efectivo').parent().hide();
+            }
 
+            if($(this).val() == 'efectivo'){
+                $('#efectivo').parent().show();
+                $('#banco').parent().hide();
+
+            }else{
+                $('#banco').parent().hide();
+                $('#efectivo').parent().hide();
             }
         })
 
