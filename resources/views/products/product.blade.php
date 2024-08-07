8d629da505d3f555f3ae1f1c9e1b2695ae1320eb
unknown <yefferson.mod@gmail.com>
2024-08-06 18:09:03 -0600

    fix list products

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -30,7 +30,7 @@
         <div class="col-md-6">
             <h2>{{ $product->t02nombre }}</h2>
             <p>{!! $product->t04descripcion !!}</p>
-            <strong><p class="red">Precio: ${{ $product->t04precio }}</p></strong>
+            <strong><p class="">Precio: ${{ $product->t04precio }}</p></strong>
             <div class="mt-3">
                 @foreach ($images as $img)
                 <img src="{{ asset($img->image_path) }}" alt="{{ $product->t02nombre }}" class="thumbnail" onclick="changeImage('{{ $img->image_path }}')" style="max-width: 100px; max-height: 100px;">
@@ -40,12 +40,11 @@
                 <form action="{{ route('cart.add') }}" method="POST">
                     @csrf
                     <input type="text" class="form-control" id="t04id" name="t04id" value="{{ $product->t04id }}" hidden >
-                    <label for="quantity">Cantidad:</label>
+                   <strong><label for="quantity">Cantidad:</label></strong>
                     <input type="number" name="quantity" id="quantity" min="1" value="1">
                     <button type="submit" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Agregar al carrito</button>
                 </form>
-
-
+Hola
                 {{-- <button type="submit" id="guardar" class="btn btn-success"><i class="fa fa-save"></i> Añadir al carrito</button> --}}
             </div>
         </div>
@@ -54,45 +53,11 @@
 
 @endsection
 @section('js')
-{{-- <script src="{{ asset('public/vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js') }}"></script> --}}
 <script>
     function changeImage(imagePath) {
         document.getElementById('mainImage').src = "{{ asset('') }}" + imagePath;
     };
 
-    // $(document).ready(function() {
-    // $("#guardar").click(function() {
-    //     const addToCartForm = document.querySelector('form[action="{{ route('cart.add') }}"]');
-
-    //         addToCartForm.addEventListener('guardar', (event) => {
-    //             event.preventDefault(); // Prevent default form submission
-
-    //             const productId = document.getElementById('t04id').value;
-    //             const quantity = document.getElementById('quantity').value;
-
-    //             // Send the product ID and quantity to the controller using an AJAX request
-    //             fetch('{{ route('cart.add') }}', {
-    //                 method: 'POST',
-    //                 headers: {
-    //                     'Content-Type': 'application/json',
-    //                     'X-CSRF-TOKEN': '{{ csrf_token() }}' // Add CSRF token for security
-    //                 },
-    //                 body: JSON.stringify({
-    //                     product_id: productId,
-    //                     quantity: quantity
-    //                 })
-    //             })
-    //                 .then(response => response.json())
-    //                 .then(data => {
-    //                     // Handle the response from the controller (e.g., display a success message)
-    //                     console.log('Producto añadido al carrito:', data);
-    //                 })
-    //                 .catch(error => {
-    //                     console.error('Error al añadir al carrito:', error);
-    //                 });
-    //         });
-    //     });
-    // });
 </script>
 
 @endsection
