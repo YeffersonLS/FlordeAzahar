300e54423025fae75462bbc97eda06033b361a80
unknown <yefferson.mod@gmail.com>
2024-08-08 15:17:31 -0600

    fix list products

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -52,18 +52,7 @@
     <div class="row">
         @foreach ($products as $product)
         <div class="col-md-6 col-lg-3">
-            <div class="">
-                <a href="{{ route('product.show', ['t04slug' => $product->t04slug]) }}">
-                    <div class="card">
-                        <img src="{{ asset($product->image_path) }}" class="card-img-top" alt="{{ $product->t02nombre }}">
-                        <div class="card-info">
-                            <p class="subcategoria-productos">{{ $product->t02nombre }}</p>
-                            <h5 class="card-title">{{ $product->t04nombre }}</h5>
-                            <p class="card-money">{{ '$' . $product->t04precio . ' MXN' }}</p>
-                        </div>
-                    </div>
-                </a>
-            </div>
+
         </div>
         @endforeach
     </div>
