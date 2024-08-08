735cdc07f1fad234517da03b4d46da803b54d2fc
unknown <yefferson.mod@gmail.com>
2024-08-08 17:13:06 -0600

    fix list products

--- app/Http/Controllers/ShowProductController.php
+++ app/Http/Controllers/ShowProductController.php
@@ -56,7 +56,7 @@ class ShowProductController extends Controller
         $images = DB::table('r03products_images')
         ->select('image_path')->where ('r03product', '=', $product->t04id)
         ->get();
-        $product = T04productos::select('t04id', 't04nombre', 't04categoria', 't04slug', 't04precio', 'i.image_path', 'c.t02nombre')
+        $products = T04productos::select('t04id', 't04nombre', 't04categoria', 't04slug', 't04precio', 'i.image_path', 'c.t02nombre')
             ->where('t04categoria', '=', $product->t04categoria)
             ->leftJoin('t02directorios as c', 'c.t02id', '=', 't04categoria')
             ->Join('r03products_images as i', function($join) {
@@ -65,7 +65,7 @@ class ShowProductController extends Controller
             })
             ->get();
 
-        return view('products.product', compact('product', 'product','images', 'image'));
+        return view('products.product', compact('products', 'product','images', 'image'));
     }
 
     /**
