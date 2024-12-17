c823b9aac51755a284bbedba537157d8f01dfdcf
unknown <yefferson.mod@gmail.com>
2024-12-16 19:27:44 -0600

    fix home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 <div class="row justify-center">
-    <div class="col-7">
+    <div class="col-8">
     @foreach($banners as $index => $banner)
         <div class="banner carousel-banner" style="background-image: url('{{ asset($banner->t06image_path) }}');">
         </div>
     @endforeach
     </div>
-    <div class="col-5">
-        <video autoplay muted loop style="width: 800px; height: 520px;">
+    <div class="col-4">
+        <video autoplay muted loop style="width: 640px; height: 800px;">
             <source src="{{ asset('public/storage/videos/navidad.mp4') }}" type="video/mp4">
         </video>
     </div>
