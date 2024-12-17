66164f54a72254565d2b6709c8d2ceb18b74763b
unknown <yefferson.mod@gmail.com>
2024-12-16 19:23:07 -0600

    fix home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     @endforeach
     </div>
     <div class="col-4">
-        <video controls style="width: 100%; height: auto;">
+        <video controls style="width: 200px; height: 200px;">
             <source src="{{ asset('public/storage/videos/navidad.mp4') }}" type="video/mp4">
         </video>
     </div>
